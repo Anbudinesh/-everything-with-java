bc37741cc260812b94d33b42ea8b82855bd50009
dineshanbu <63343011+Anbudinesh@users.noreply.github.com>
2022-01-12 14:55:05 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    int n= 33445;
+    int count = 0;
+    while(n>0)
+{
+  int rem = n%10;
+  if (rem==3 || rem==4)
+  {
+    count++;
+  }
+  n=n/10;
+
+}
+  System.out.println(count);
+  }
+}
